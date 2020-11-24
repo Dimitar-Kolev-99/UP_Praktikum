ea25686d3d57adb0d2522b053b68843b2ef556aa
DimitarKolevSI <dimitarrukolev@gmail.com>
2020-11-24 19:37:22 +0200

    Task 2

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <climits>
 
 using namespace std;
 
+//Task 1
 int sum(int arr[], int n, int index) {
 	if (index == n) {
 		return 0;
@@ -9,10 +11,25 @@ int sum(int arr[], int n, int index) {
 	return arr[index] + sum(arr, n, index + 1);
 }
 
+//Task2
+int findElement(int arr[], int n, int index, int element) {
+	if (n == index) {
+		return INT_MIN;
+	}
+	else if (arr[index] == element) {
+		return index;
+	}
+	else {
+		return findElement(arr, n, index + 1, element);
+	}
+}
+
 int main() {
-	const int n = 5;
-	int arr[n] = { 5,3,6,2,1 };
-	cout << sum(arr, n, 0) << endl;
+	const int n = 10;
+	int arr[n] = { 1,2,3,4,5,6,7,8,9,10 };
+
+	cout << findElement(arr, n, 0, 5) << endl;
+	cout << findElement(arr, n, 0, -1) << endl;
 	system("pause");
 	return 0;
 }
